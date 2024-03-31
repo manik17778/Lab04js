23d6eea9d33515bd9dc155cb2f4b3ae673d294e9
manik17778 <137090327+manik17778@users.noreply.github.com>
2024-03-30 20:23:15 -0400

    updated

--- js/json.js
+++ js/json.js
@@ -7,7 +7,7 @@ async function populate() {
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4a: Create i-scream.json file with companyName, headOffice, established, active, topFlavors(name, calories, type, ingredients, image) */
     // STEP 4b: Store the URL of a JSON file in a variable */
-    const url = "https://priyansht.github.io/24W-JavaScript-03-Week11/js/i-scream.json";
+    const url = "https://manik17778.github.io/Lab04js/js/i-scream.json"; // adding my github  live link 
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(url);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
